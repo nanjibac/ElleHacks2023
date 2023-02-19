7803b2b1fdff8ea33d19675a2d180003206925b8
laibaz16 <laibazahid16@gmail.com>
2023-02-19 03:27:53 -0500

    reading datasets

---
+++ WeatherPattern/weatherchart.js
@@ -0,0 +1,16 @@
+//** */
+//const myForm = document.getElementById("myForm");
+//const csvFile = document.getElementById("csvFile");
+
+
+//myForm.addEventListener("submit", function (e) {
+ // console.log("hellow world")
+  // e.preventDefault();
+  // const input = csvFile.files[0];
+  // const reader = new FileReader();
+  // reader.onload = function (e) {
+  //    const text = e.target.result;
+  //    reader.readAsText(file);
+  // };
+  // reader.readAsText(input);
+//});
